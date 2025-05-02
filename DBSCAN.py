3204dba81a08bf1bf3611036a6f1ad70876a77ce
givemeyourhatplease <rogerline0527@gmail.com>
2025-05-02 22:47:54 +0800

    Define the structure of DBSCAN preliminary

---
+++ DBSCAN.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class DBSCAN:
+    def __init__(self, data):
+        self.data = data
+        self.clusters = []
+
+    def fit(self, eps, min_points):
+
+        pass
+
+    def predict(self, data):
+
+        pass
