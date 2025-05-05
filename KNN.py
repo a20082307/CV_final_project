01d6f84b64b9f6da4e5150c93554e85ac9044dad
Wei De Chien <wade00754@gmail.com>
2025-05-05 21:07:15 +0800

    simple implement of knn

---
+++ KNN.py
@@ -0,0 +1,60 @@
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+
+
+class KNearestNeighbors:
+    """A simple implementation of the K-Nearest Neighbors (KNN) classifier.
+
+    Attributes:
+        k (int, default=5): Number of nearest neighbors to consider.
+    """
+
+    def __init__(self, k=5):
+        self.k = k
+
+    def fit(self, X, y):
+        """Fit the KNN classifier to the training data. (lazy learning)"""
+        self.X_train = X
+        self.y_train = y
+
+    def predict(self, X):
+        """Predict the class labels for the provided data.
+
+        Args:
+            X (array-like): The input data to predict.
+
+        Returns:
+            ndarray: Predicted class labels for each sample in X.
+        """
+        predictions = [self._predict(x) for x in X]
+        return np.array(predictions)
+
+    def score(self, X, y):
+        """Compute the accuracy of the classifier on the provided data.
+
+        Args:
+            X (array-like): The input data to score.
+            y (array-like): True class labels for the input data.
+
+        Returns:
+            float: The accuracy of the classifier on the provided data.
+        """
+        predictions = self.predict(X)
+        accuracy = np.mean(predictions == y)
+        return accuracy
+
+    def _predict(self, x):
+        """Predict the class label for a single sample.
+
+        Args:
+            x (array-like): The input data to predict.
+
+        Returns:
+            int: The predicted class label for the input data.
+        """
+        distances = np.linalg.norm(self.X_train - x, axis=1)
+        k_indices = np.argsort(distances)[: self.k]
+        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        most_common = np.bincount(k_nearest_labels).argmax()
+        return most_common
