585f668043cce30eab1fb803fc767423c7f981b6
givemeyourhatplease <rogerline0527@gmail.com>
2025-05-15 22:36:55 +0800

    Correct the logic of fit(), now it acts same as sklearn.cluster.DBSCAN

--- DBSCAN.py
+++ DBSCAN.py
@@ -1,84 +1,103 @@
-import copy
 import random
 
 import numpy as np
 import scipy as sp
 import sklearn
 import sklearn.cluster
+import sklearn.decomposition
+import tqdm
 
 from main import data_loader, PCA
 
 
 class DBSCAN:
     def __init__(self, data, label, eps, min_points, dimension):
-        self.data = data
+        if dimension > 0:
+            self.data = PCA(data, dimension)
+        else:
+            self.data = data
+
         self.label = label
         self.eps = eps
         self.min_points = min_points
-        self.dim = dimension
         
+        self.data_num = self.data.shape[1]
         self.index = [i for i in range(data.shape[1])]
         self.clusters = sp.cluster.hierarchy.DisjointSet(self.index)
         self.noise = set()
 
     def fit(self):
-        if self.dim > 0:
-            self.data = PCA(self.data, self.dim)
-
         points_without_cluster = set(self.index)
-        select_order = copy.deepcopy(self.index)
         
-        random.seed(110062333)
-        random.shuffle(select_order)
+        with tqdm.tqdm(total = self.data_num, desc = '[Clustering]') as pbar:
+            for cur_point in range(self.data_num):
+                pbar.update(1)
 
-        print('clustering...')
-        while len(points_without_cluster):
-            random_index = select_order.pop()
-            if not random_index in points_without_cluster:
-                continue
+                if cur_point not in points_without_cluster:
+                    continue
 
-            random_point = self.data[:, random_index].reshape(-1, 1)
-            points_without_cluster.remove(random_index)
+                points_without_cluster.remove(cur_point)
 
-            diff = random_point - self.data
-            dist = np.linalg.norm(diff, axis = 0)
-            dist = dist[dist > 0]
-            # print(np.max(dist), np.min(dist))
+                diff = self.data[:, cur_point].reshape(-1, 1) - self.data
+                dist = np.linalg.norm(diff, axis = 0)
+                neighbors = np.where(dist <= self.eps)[0]
 
-            neighbors = np.where(dist < self.eps)[0]
-            if neighbors.shape[0] >= self.min_points:
-                # print(neighbors.shape, len(points_without_cluster))
-                for neighbor in neighbors:
-                    if neighbor in points_without_cluster:
-                        points_without_cluster.remove(neighbor)
-                    elif neighbor in self.noise:
+                if len(neighbors) < self.min_points:
+                    self.noise.add(cur_point)
+                    continue
+
+                while len(neighbors):
+                    neighbor = neighbors[0]
+                    neighbors = np.delete(neighbors, 0)
+
+                    if neighbor not in points_without_cluster and neighbor not in self.noise:
+                        continue
+
+                    if neighbor in self.noise:
                         self.noise.remove(neighbor)
-                    self.clusters.merge(random_index, neighbor)
-            else:
-                self.noise.add(random_index)
-        
+                    elif neighbor in points_without_cluster:
+                        points_without_cluster.remove(neighbor)
+
+                    self.clusters.merge(cur_point, neighbor)
+                    
+                    n_diff = self.data[:, neighbor].reshape(-1, 1) - self.data
+                    n_dist = np.linalg.norm(n_diff, axis = 0)
+                    neighbor_neighbors = np.where(n_dist <= self.eps)[0]
+
+                    if len(neighbor_neighbors) < self.min_points:
+                        continue
+
+                    for nn in neighbor_neighbors:
+                        if nn in neighbors:
+                            continue
+
+                        neighbors = np.hstack([neighbors, nn])
+                        
         cluster_size = np.array([len(cluster) for cluster in self.clusters.subsets()])
-        valid_cluster = np.where(cluster_size > self.min_points)[0]
+        valid_cluster = np.where(cluster_size > 1)[0]
 
         print(f'There are {valid_cluster.shape[0]} clusters and {len(self.noise)} noise points')
         for i in range(len(valid_cluster)):
-            print(f'Cluster {i} has {len(self.clusters.subsets()[valid_cluster[i]])} elements')
+            print(f'Cluster {i + 1} has {len(self.clusters.subsets()[valid_cluster[i]])} elements')
 
-            
     def predict(self, data):
-
-
-
         raise NotImplementedError
+
 
 if __name__ == '__main__':
     train_data, train_label, _, _ = data_loader(24, 2)
     # print(np.sum(train_label == 1), np.sum(train_label == 0), len(train_label))
 
-    model = DBSCAN(train_data, train_label, 4, 30, -1)
+    model = DBSCAN(train_data, train_label, 0.05, 20, 2)
     model.fit()
 
-    db = sklearn.cluster.DBSCAN(eps=4, min_samples=30, metric='euclidean')
-    clusters = db.fit_predict(train_data.T)
-    print(f"Number of clusters: {len(np.unique(clusters[clusters != -1]))}")
-    print(f"Number of noise points: {np.sum(clusters == -1)}")
+    # train_data = PCA(train_data, 2).T
+    # print(train_data.shape)
+    # db = sklearn.cluster.DBSCAN(eps = 0.05, min_samples = 20, metric='euclidean')
+    # clusters = db.fit_predict(train_data)
+
+    # cluster_num = len(np.unique(clusters[clusters != -1]))
+    # print(f"Number of clusters: {cluster_num}")
+    # print(f"Number of noise points: {np.sum(clusters == -1)}")
+    # for i in range(cluster_num):
+    #     print(len(clusters[clusters == i]))
