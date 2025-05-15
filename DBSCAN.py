6429dc070fb025009c09df53aedb3c73cdb9f3e6
givemeyourhatplease <rogerline0527@gmail.com>
2025-05-15 23:10:30 +0800

    Now each cluster has its own meaning, including (Rise/Fall/Not Sure, rise_prob/fall_prop/0.5)

--- DBSCAN.py
+++ DBSCAN.py
@@ -24,6 +24,7 @@ class DBSCAN:
         self.data_num = self.data.shape[1]
         self.index = [i for i in range(data.shape[1])]
         self.clusters = sp.cluster.hierarchy.DisjointSet(self.index)
+        self.cluster_meaning = []
         self.noise = set()
 
     def fit(self):
@@ -73,12 +74,28 @@ class DBSCAN:
 
                         neighbors = np.hstack([neighbors, nn])
                         
-        cluster_size = np.array([len(cluster) for cluster in self.clusters.subsets()])
+        all_clusters = self.clusters.subsets()
+        cluster_size = np.array([len(cluster) for cluster in all_clusters])
         valid_cluster = np.where(cluster_size > 1)[0]
+        self.clusters = [all_clusters[i] for i in valid_cluster]
 
-        print(f'There are {valid_cluster.shape[0]} clusters and {len(self.noise)} noise points')
-        for i in range(len(valid_cluster)):
-            print(f'Cluster {i + 1} has {len(self.clusters.subsets()[valid_cluster[i]])} elements')
+        # print(f'There are {valid_cluster.shape[0]} clusters and {len(self.noise)} noise points')
+        # for i in range(len(self.clusters)):
+        #     print(f'Cluster {i + 1} has {len(self.clusters[i])} elements')
+
+        for index in valid_cluster:
+            cluster = all_clusters[index]
+            rise_points = [i for i in cluster if self.label[i]]
+            rise_prob = len(rise_points) / len(cluster)
+            
+            if rise_prob > 0.5:
+                self.cluster_meaning.append(('Rise', rise_prob))
+            elif rise_prob < 0.5:
+                self.cluster_meaning.append(('Fall', 1 - rise_prob))
+            else:
+                self.cluster_meaning.append(('Not Sure', 0.5))
+
+        print(self.cluster_meaning)
 
     def predict(self, data):
         raise NotImplementedError
